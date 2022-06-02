02c57c21d043a1ac4a45b9524a2488e02bef7d99
Ren Tatsumoto <tatsu@autistici.org>
2022-06-02 08:35:56 +0300

    refactor libwebp downloading

---
+++ libwebp-dl.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# https://developers.google.com/speed/webp/download
+readonly version=1.2.2
+readonly webp_windows="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-${version}-windows-x64.zip"
+readonly webp_linux="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-${version}-linux-x86-64.tar.gz"
+readonly webp_mac="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-${version}-mac-x86-64.tar.gz"
+
+readonly support_dir=$(dirname -- "$(readlink -f -- "$0")")/support
+readonly tmp_dir=$support_dir/temp
+
+get_libwebp() {
+	local -r url=${1:?} file_path=$tmp_dir/${1##*/}
+	if ! [[ -f $file_path ]]; then
+		curl -s --output "$file_path" -- "$url"
+	fi
+	atool -f -X "$tmp_dir/" -- "$file_path"
+}
+
+if ! [[ -f $support_dir/cwebp && -f $support_dir/cwebp.exe && -f $support_dir/cwebp_macos ]]; then
+	mkdir -p -- "$tmp_dir"
+	for url in "$webp_windows" "$webp_linux" "$webp_mac"; do
+		get_libwebp "$url" &
+	done
+	wait
+	find "$tmp_dir"/*windows* -type f  -name 'cwebp.exe' -exec mv -- {} "$support_dir/" \;
+	find "$tmp_dir"/*linux*   -type f  -name 'cwebp'     -exec mv -- {} "$support_dir/" \;
+	find "$tmp_dir"/*mac*     -type f  -name 'cwebp'     -exec mv -- {} "$support_dir/cwebp_macos" \;
+	rm -rf -- "$tmp_dir"
+fi
--- package.sh
+++ package.sh
@@ -5,48 +5,27 @@ readonly target=${1:?Please provide build target: \"ankiweb\" or \"github\"}
 readonly branch=${2:-$(git branch --show-current)}
 readonly addon_name="Paste Images As WebP"
 readonly package_filename="${addon_name// /}_${branch}.ankiaddon"
-readonly support_dir="support"
+readonly support_dir=support
 readonly manifest=manifest.json
-
-# https://developers.google.com/speed/webp/download
-readonly webp_windows="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.2.0-windows-x64.zip"
-readonly webp_linux="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.2.0-linux-x86-64.tar.gz"
-readonly webp_mac="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.2.0-mac-10.15.tar.gz"
 
 cd -- "$root_dir" || exit 1
 rm -- "$package_filename" 2>/dev/null
 export root_dir branch
 
-
-if [[ "$target" != 'ankiweb' ]]; then
-    # https://addon-docs.ankiweb.net/#/sharing?id=sharing-outside-ankiweb
-    # If you wish to distribute .ankiaddon files outside of AnkiWeb,
-    # your add-on folder needs to contain a ‘manifest.json’ file.
-    {
-        echo '{'
-        echo -e "\t\"package\": \"${package_filename%.*}\","
-        echo -e "\t\"name\": \"$addon_name\","
-        echo -e "\t\"mod\": $(date -u '+%s')"
-        echo '}'
-    } > $manifest
+if [[ $target != ankiweb ]]; then
+	# https://addon-docs.ankiweb.net/sharing.html
+	# If you wish to distribute .ankiaddon files outside of AnkiWeb,
+	# your add-on folder needs to contain a ‘manifest.json’ file.
+	cat <<- EOF > $manifest
+	{
+		"package": "${package_filename%.*}",
+		"name": "$addon_name",
+		"mod": $(date -u '+%s')
+	}
+	EOF
 fi
 
-if ! [[ -f ./$support_dir/cwebp && -f ./$support_dir/cwebp.exe && -f ./$support_dir/cwebp_macos ]]; then
-	readonly tmp_dir=./$support_dir/temp/
-	mkdir -p -- $tmp_dir
-	for url in "$webp_windows" "$webp_linux" "$webp_mac"; do
-		filename=${url##*/}
-		if [[ ! -f $tmp_dir/$filename ]]; then
-			curl --output $tmp_dir/$filename -- "$url"
-		fi
-		atool -f -X $tmp_dir/ -- $tmp_dir/$filename
-	done
-	unset filename
-	find $tmp_dir/*windows* -type f  -name 'cwebp.exe' -exec mv -- {} ./$support_dir/ \;
-	find $tmp_dir/*linux*   -type f  -name 'cwebp'     -exec mv -- {} ./$support_dir/ \;
-	find $tmp_dir/*mac*     -type f  -name 'cwebp'     -exec mv -- {} ./$support_dir/cwebp_macos \;
-	rm -rf $tmp_dir
-fi
+bash ./libwebp-dl.sh
 
 git archive "$branch" --format=zip --output "$package_filename"
 zip -ur "$package_filename" ./"$manifest" ./$support_dir/cwebp*
