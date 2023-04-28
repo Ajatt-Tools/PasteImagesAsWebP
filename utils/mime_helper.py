daada54e52abdf0ec15afa7b632941ff4d2e6468
Ren Tatsumoto <tatsu@autistici.org>
2023-04-28 05:59:06 +0300

    refactor utils

--- utils/mime_helper.py
+++ utils/mime_helper.py
@@ -17,7 +17,7 @@
 # Any modifications to this file must keep this entire header intact.
 
 import re
-from typing import Optional, Iterable, List, Tuple
+from typing import Optional, Iterable
 
 import requests
 from aqt.qt import *
@@ -34,11 +34,11 @@ def data_from_html(html: str) -> list[QByteArray]:
     return [QByteArray.fromBase64(data.encode('ascii')) for data in re.findall('(?<=;base64,)[^"]+(?=")', html)]
 
 
-def urls(mime: QMimeData) -> Iterable[str]:
+def iter_urls(mime: QMimeData) -> Iterable[str]:
     return (url.toString() for url in mime.urls() if not url.isLocalFile())
 
 
-def files(mime: QMimeData) -> Iterable[str]:
+def iter_files(mime: QMimeData) -> Iterable[str]:
     return (url.toLocalFile() for url in mime.urls() if url.isLocalFile())
 
 
@@ -61,9 +61,9 @@ def image_candidates(mime: QMimeData) -> Iterable[Optional[QImage]]:
     yield mime.imageData()
     for data in data_from_html(mime.html()):
         yield QImage.fromData(data)
-    for file in files(mime):
+    for file in iter_files(mime):
         yield image_from_file(file)
-    for url in urls(mime):
+    for url in iter_urls(mime):
         yield image_from_url(url)
     for url in urls_from_html(mime.html()):
         yield image_from_url(url)
