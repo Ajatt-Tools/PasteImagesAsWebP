14116153f08cbd27c615c5d149b2a9d580ac50f2
Shou Ya <shouya@users.noreply.github.com>
2024-02-17 12:13:56 +0900

    allow customizing whether or not to show the dialog for pasting

--- utils/show_options.py
+++ utils/show_options.py
@@ -5,6 +5,7 @@ class ShowOptions(enum.Enum):
     menus = "Toolbar and menus"
     drag_and_drop = "On drag and drop"
     add_note = "Note added"
+    paste = "On paste"
 
     def __eq__(self, other: str):
         return self.name == other
