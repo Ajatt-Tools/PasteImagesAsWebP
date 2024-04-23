51c427ce484d277a7032d73dacd25ee7a143cf6b
Ren Tatsumoto <tatsu@autistici.org>
2024-04-23 13:33:30 +0300

    update package script

--- libwebp-dl.sh
+++ libwebp-dl.sh
@@ -2,13 +2,22 @@
 
 # https://developers.google.com/speed/webp/download
 
+readonly NC='\033[0m'
+readonly RED='\033[0;31m'
+readonly GREEN='\033[0;32m'
+
 readonly version=1.4.0
 readonly webp_windows="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-${version}-windows-x64.zip"
 readonly webp_linux="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-${version}-linux-x86-64.tar.gz"
 readonly webp_mac="https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-${version}-mac-x86-64.tar.gz"
 
-readonly support_dir=$(dirname -- "$(readlink -f -- "$0")")/support
+readonly support_dir=support
 readonly tmp_dir=$support_dir/temp
+
+if ! command -v atool >/dev/null; then
+	echo -e "${RED}atool is not installed!${NC}"
+	exit 1
+fi
 
 get_libwebp() {
 	local -r url=${1:?} file_path=$tmp_dir/${1##*/}
@@ -18,14 +27,19 @@ get_libwebp() {
 	atool -f -X "$tmp_dir/" -- "$file_path"
 }
 
-if ! [[ -f $support_dir/cwebp && -f $support_dir/cwebp.exe && -f $support_dir/cwebp_macos ]]; then
-	mkdir -p -- "$tmp_dir"
-	for url in "$webp_windows" "$webp_linux" "$webp_mac"; do
-		get_libwebp "$url" &
-	done
-	wait
-	find "$tmp_dir"/*windows* -type f  -name 'cwebp.exe' -exec mv -- {} "$support_dir/cwebp.exe" \;
-	find "$tmp_dir"/*linux*   -type f  -name 'cwebp'     -exec mv -- {} "$support_dir/cwebp.lin" \;
-	find "$tmp_dir"/*mac*     -type f  -name 'cwebp'     -exec mv -- {} "$support_dir/cwebp.mac" \;
-	rm -rf -- "$tmp_dir"
+if [[ -f $support_dir/cwebp.lin && -f $support_dir/cwebp.exe && -f $support_dir/cwebp.mac ]]; then
+	echo "cwebp is already downloaded."
+	exit
 fi
+
+mkdir -p -- "$tmp_dir"
+echo "downloading cwebp..."
+for url in "$webp_windows" "$webp_linux" "$webp_mac"; do
+	get_libwebp "$url" &
+done
+wait
+find "$tmp_dir"/*windows* -type f  -name 'cwebp.exe' -exec mv -- {} "$support_dir/cwebp.exe" \;
+find "$tmp_dir"/*linux*   -type f  -name 'cwebp'     -exec mv -- {} "$support_dir/cwebp.lin" \;
+find "$tmp_dir"/*mac*     -type f  -name 'cwebp'     -exec mv -- {} "$support_dir/cwebp.mac" \;
+rm -rf -- "$tmp_dir"
+echo "downloaded cwebp."
--- package.sh
+++ package.sh
@@ -1,51 +1,19 @@
 #!/bin/bash
 
+set -euo pipefail
+
 readonly NC='\033[0m'
 readonly RED='\033[0;31m'
 readonly GREEN='\033[0;32m'
 
-for exe in git zip zipmerge; do
-	if ! command -v "$exe" >/dev/null; then
-		echo -e "${RED}Missing dependency:${NC} $exe"
-		exit 1
-	fi
-done
-
-readonly root_dir=$(git rev-parse --show-toplevel)
-readonly target=${1:?Please provide build target: \"ankiweb\" or \"github\"}
-readonly branch=${2:-$(git branch --show-current)}
-readonly addon_name="Paste Images As WebP"
-readonly package_filename="${addon_name// /}_${branch}.ankiaddon"
 readonly support_dir=support
-readonly manifest=manifest.json
+package=PasteImagesAsWebP
 
-cd -- "$root_dir" || exit 1
-rm -- "$package_filename" 2>/dev/null
-export root_dir branch
+./ajt_common/package.sh \
+	--package "$package" \
+	--name "AJT Paste Images As WebP" \
+	"$@"
 
-if [[ $target != ankiweb ]]; then
-	# https://addon-docs.ankiweb.net/sharing.html
-	# If you wish to distribute .ankiaddon files outside of AnkiWeb,
-	# your add-on folder needs to contain a ‘manifest.json’ file.
-	cat <<- EOF > $manifest
-	{
-		"package": "${package_filename%.*}",
-		"name": "$addon_name",
-		"mod": $(date -u '+%s')
-	}
-	EOF
-fi
-
-bash ./libwebp-dl.sh
-
-git archive "$branch" --format=zip --output "$package_filename"
-zip -ur "$package_filename" ./"$manifest" ./$support_dir/cwebp*
-
-# shellcheck disable=SC2016
-git submodule foreach 'git archive HEAD --prefix=$path/ --format=zip --output "$root_dir/${path}_${branch}.zip"'
-
-zipmerge "$package_filename" ./*.zip
-
-rm -- $manifest 2>/dev/null
-rm -- ./*.zip
-echo -e "${GREEN}Done.${NC}"
+./libwebp-dl.sh
+zip -ur "${package,,}.ankiaddon" ./$support_dir/cwebp*
+echo -e "${GREEN}Added cwebp binaries.${NC}"
