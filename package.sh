eef115d8d14ec79d5b7d9c01bdfd4992b2906412
Ren Tatsumoto <tatsu@autistici.org>
2024-04-23 13:36:22 +0300

    check if ankiaddon file is created

--- package.sh
+++ package.sh
@@ -7,13 +7,19 @@ readonly RED='\033[0;31m'
 readonly GREEN='\033[0;32m'
 
 readonly support_dir=support
-package=PasteImagesAsWebP
+readonly package=PasteImagesAsWebP
+readonly zip_name=${package,,}.ankiaddon
 
 ./ajt_common/package.sh \
 	--package "$package" \
 	--name "AJT Paste Images As WebP" \
 	"$@"
 
+if ! [[ -f "$zip_name" ]]; then
+	echo -e "${RED}Missing file:${NC} $zip_name"
+	exit 1
+fi
+
 ./libwebp-dl.sh
-zip -ur "${package,,}.ankiaddon" ./$support_dir/cwebp*
+zip -ur "$zip_name" ./$support_dir/cwebp*
 echo -e "${GREEN}Added cwebp binaries.${NC}"
