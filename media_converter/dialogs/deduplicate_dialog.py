aea79a2339b98964e19940cc46511075d0393543
Ren Tatsumoto <tatsu@autistici.org>
2026-02-25 01:04:50 +0300

    add context menu action: Search in Browser

--- media_converter/dialogs/deduplicate_dialog.py
+++ media_converter/dialogs/deduplicate_dialog.py
@@ -4,7 +4,9 @@
 import typing
 from collections.abc import Sequence
 
+import aqt
 from aqt.qt import *
+from aqt.utils import tooltip
 
 from ..ajt_common.stats_table_dialog import StatsDialog
 from ..ajt_common.utils import ui_translate
@@ -26,10 +28,43 @@ class DeduplicateMediaConfirmDialog(StatsDialog):
         QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
     )
 
-    def __init__(self, column_names: Sequence[str], parent=None) -> None:
+    def __init__(self, column_names: Sequence[str], parent: typing.Optional[aqt.AnkiQt] = None) -> None:
         super().__init__(column_names=column_names, parent=parent)
         self._count_label = QLabel()
         self._layout.insertWidget(0, self._count_label)
+        self._setup_context_menu()
+
+    def _setup_context_menu(self) -> None:
+        """
+        Create right-click context menu actions. The user can copy the contents of a cell to the clipboard.
+        """
+        self._table.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        qconnect(self._table.customContextMenuRequested, self._show_context_menu)
+
+    def _show_context_menu(self, pos: QPoint) -> None:
+        item = self._table.itemAt(pos)
+        if not item:
+            return
+        menu = QMenu(self._table)
+
+        search_action = menu.addAction("Search in Browser")
+        search_action.setEnabled(item is not None)
+        qconnect(search_action.triggered, lambda: self._search_in_anki_browser(item))
+
+        menu.exec(self._table.viewport().mapToGlobal(pos))
+
+    def _search_in_anki_browser(self, item: typing.Optional[QTableWidgetItem]) -> None:
+        """
+        Paste selected text into the Browser's search bar and perform search.
+        """
+        if not aqt.mw:
+            # Can't do anything with Anki. Abort.
+            return
+        if not (item and item.text()):
+            tooltip("Empty selection.", parent=self)
+        browser = aqt.dialogs.open("Browser", aqt.mw)  # browser requires mw (AnkiQt) to be passed as parent
+        browser.activateWindow()
+        browser.search_for(item.text())
 
     def row_count(self) -> int:
         return self._table.rowCount()
