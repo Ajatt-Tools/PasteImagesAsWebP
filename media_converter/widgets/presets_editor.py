bb14e5d72e410f0e9fa99e9eafbd0b9090e45db9
Ren Tatsumoto <tatsu@autistici.org>
2026-05-22 07:40:30 +0300

    fix import

--- media_converter/widgets/presets_editor.py
+++ media_converter/widgets/presets_editor.py
@@ -1,6 +1,6 @@
 # Copyright: Ren Tatsumoto <tatsu at autistici.org>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-from typing import Sequence
+from collections.abc import Sequence
 
 from aqt.qt import *
 
