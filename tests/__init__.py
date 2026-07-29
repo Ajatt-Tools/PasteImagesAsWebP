17b9830ea47b027f6fbb0aa7be2e3b702d19191a
Ren Tatsumoto <tatsu@autistici.org>
2026-07-29 17:39:59 +0300

    update pyproject

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,7 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+# https://github.com/beartype/beartype
+from beartype.claw import beartype_this_package
+
+beartype_this_package()
